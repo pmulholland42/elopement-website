f58be16c4ae56f931c1afe1e12e4bf7f84295969
Peter Mulholland <pmulholland08@gmail.com>
2025-03-13 08:37:02 -0400

    Add db setup

--- src/app/manage/actions.ts
+++ src/app/manage/actions.ts
@@ -37,6 +37,31 @@ export const addInviteeAction = async (partyInfo: PartyInfo) => {
 
   const db = getDatabaseConnection();
 
+  await db.any(`
+    CREATE TABLE IF NOT EXISTS party (
+    id TEXT DEFAULT left(md5(random()::text), 8) PRIMARY KEY,
+    name TEXT NOT NULL,
+	completed BOOLEAN DEFAULT false,
+	contact TEXT DEFAULT 'Jacqui'
+);
+
+CREATE TABLE IF NOT EXISTS invitee (
+    id TEXT DEFAULT left(md5(random()::text), 8) PRIMARY KEY,
+    party_id TEXT NOT NULL,
+    name TEXT,
+	alt_name TEXT,
+    accepted BOOLEAN,
+	created_at TIMESTAMPTZ DEFAULT NOW(),
+    FOREIGN KEY (party_id) REFERENCES party(id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS cookie_response (
+	id TEXT DEFAULT left(md5(random()::text), 8) PRIMARY KEY,
+	name TEXT,
+	cookies TEXT
+);
+`);
+
   await db.tx("add-invitee-tx", async (t) => {
     console.log("addInviteeAction tx");
     const insParty = await t.one(
